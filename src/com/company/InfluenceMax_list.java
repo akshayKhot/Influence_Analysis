00bcc953f5b61c8b8c93b80dc09100ce991a89a8
Akshay Khot <akshay.7277@gmail.com>
2017-02-21 14:36:25 -0800

    add revised code

--- src/com/company/InfluenceMax_list.java
+++ src/com/company/InfluenceMax_list.java
@@ -1,53 +1,37 @@
 package com.company;
+
 import java.util.ArrayDeque;
 import java.util.BitSet;
 import java.util.Deque;
 import java.util.Random;
+import java.util.List;
+import java.util.ArrayList;
 
-import java.util.Arrays;
 
 import it.unimi.dsi.webgraph.ImmutableGraph;
 
-public class InfluenceMax_flat {
+public class InfluenceMax_list {
     ImmutableGraph G;
     int n;
     long m;
     double eps = .1;
     double p; // given probability of arc existence
-    int k; // given number of maximum influence nodes
-    int nMAX = 1000000000;
+    int k = 5; // given number of maximum influence nodes
 
     int[] permutation;
     BitSet marked;
 
-    // Flat arrays to keep: sketches - all sketch numbers in order, like 0000...1111...2222...;
-    //                      nodes - all corresponding node IDs participating in sketch, like 13 17 100 230 ... 13 23 300 10000 ...
-    //                      node_infl - counts of sketches the node is participating into;
-    int[] sketches;
-    int[] nodes;
-    int[] node_infl;
-
-    int count_sketches; // the length of sketches and nodes arrays
-
-    public InfluenceMax_flat(String basename, Double  p) throws Exception {
+    public InfluenceMax_list(String basename, Double  p) throws Exception {
         G = ImmutableGraph.load(basename);
 
         n = G.numNodes();
         m = G.numArcs();
-        k = 5;
         //System.out.println("n="+n + ", m=" +m  + ", W=" +( (1/Math.pow(eps, 3)) * (n+m)*Math.log(n)  ));
         System.out.println("n="+n + ", m=" +m  + ", W=" +( 32 * (n+m)*Math.log(n)  ));
-
 
         marked = new BitSet(n);
 
         permutation = new int[n];
-
-        // Flat arrays created: sketches and nodes are very large, nMAX equals one billion
-        //                      node_infl - the size is n (the number of nodes)
-        sketches = new int[nMAX];
-        nodes = new int[nMAX];
-        node_infl = new int[n];
 
 //		System.out.println("Initializing the permutation array...");
         for(int i=0; i<n; i++)
@@ -66,15 +50,6 @@ public class InfluenceMax_flat {
         }
         System.out.println("Permutation array shuffled.");
 
-        System.out.println("Initializing index I...");
-        for(int i=0;i<nMAX;i++)
-        {
-            sketches[i] = -1;
-            nodes[i] = -1;
-        }
-
-        System.out.println("index I initialized.");
-
 
         this.p = p;
     }
@@ -84,12 +59,19 @@ public class InfluenceMax_flat {
         //double W = (1/Math.pow(eps, 3)) * (n + m) * Math.log(n);
         double W = 32 * (n + m) * Math.log(n);
 
+
+        List<List<Integer>> I = new ArrayList<List<Integer>>();
+        for(int j=0;j<n;j++)
+        {
+            I.add(new ArrayList<Integer>());
+        }
+        System.out.println("index I created.");
+
         double weight_of_current_index = 0.0;
         int index = 0;
         int sketch_num = 0;
 
         long startTime = System.currentTimeMillis();
-        count_sketches = 0;
         while(weight_of_current_index < W)
         {
             if(index % 100000 == 0) {
@@ -109,41 +91,32 @@ public class InfluenceMax_flat {
             marked.clear();
             BFS(v,marked);
 
+            int j=0;
             int total_out_degree = 0;
-            int iteration = 0;
             for (int u = marked.nextSetBit(0); u >= 0; u = marked.nextSetBit(u+1))
             {
-                sketches[count_sketches + iteration] = sketch_num;
-                nodes[count_sketches + iteration]  = u;
-                node_infl[u] = node_infl[u] + 1;
-                iteration = iteration + 1;
+                I.get(u).add(sketch_num);
                 total_out_degree += G.outdegree(u);
             }
-
             weight_of_current_index += (marked.cardinality() + total_out_degree);
             index = ( index + 1 ) % n;
             sketch_num++;
-            count_sketches += marked.cardinality();
         }
-
         System.out.println();
         System.out.println("Index: " + index +
-                ", Number of Sketches: " + sketch_num +
-                ", Size of array iSketch: " + count_sketches);
+                " Number of Sketches: " + sketch_num);
         System.out.println();
 
-        // Cutting off the tails of sketches and nodes arrays, making the arrays shorter
-        int[] iSketch = new int[count_sketches+1];
-        System.arraycopy(sketches,0,iSketch,0,count_sketches);
-
-        int[] iNode = new int[count_sketches+1];
-        System.arraycopy(nodes,0,iNode,0,count_sketches);
-
-        int sketch_left = sketch_num;
-
+/*        for(int j=0;j<25;j++)
+        {
+            System.out.println(I.get(j));
+            System.out.println();
+        }
+ */
         System.gc();
-        get_seeds(iSketch, iNode, node_infl, k, count_sketches, sketch_num, sketch_left);
+        get_seeds(I, k, sketch_num);
     }
+
 
     void BFS(int v, BitSet marked) {
 
@@ -171,30 +144,29 @@ public class InfluenceMax_flat {
         }
     }
 
-    void get_seeds(int[] sketch_I, int[] node_I, int[] node_infl, int k, int count_sketches, int sketch_num, int sketch_left) {
+    void get_seeds(List<List<Integer>> I, int k, int sketch_num) {
 
-        // Calculating the node with max influence
         int infl_max = 0;
         int max_node = 0;
+        int node_sketch = 1;
+
 
         for(int v=0;v<n;v++)
         {
-            if(node_infl[v] == 0)
+            if(I.get(v).size() < node_sketch)
                 continue;
-            else
-            {
-                if(node_infl[v] * n / sketch_num > infl_max)
-                {
-                    infl_max = node_infl[v] * n / sketch_num;
+            else {
+                if(I.get(v).size() * n/sketch_num > infl_max) {
+                    infl_max = I.get(v).size() * n/sketch_num;
                     max_node = v;
                 }
             }
         }
 
         System.out.println();
-        System.out.println("Max Node = " + max_node +
-                ", Maximum Influence = " + infl_max + " nodes" +
-                ", Sketches left = " + sketch_left);
+        System.out.println(
+                "Max Node = " + max_node +
+                        ", Maximum Influence = " + infl_max);
         System.out.println();
 
         // Stopping condition: no need to re-calculate the influence, if we already got the k seeds
@@ -203,43 +175,18 @@ public class InfluenceMax_flat {
 
         // Re-calculating the influence of the remaining nodes: remove max node and the sketches it participated in
         // plus re-calculate the influence
-        for(int j=0;j<count_sketches;j++)
-        {
-            if(node_I[j] == -1)
+
+        for(int u=0;u<n;u++) {
+            if((I.get(u).size() < node_sketch) || (u == max_node))
                 continue;
             else
             {
-                if(node_I[j] == max_node)
-                {
-                    int redundant_sketch = sketch_I[j];
-
-                    // As sketches are added to the array in numerical order, the same redundant sketch can be found before and after the max node
-                    int l = j+1;
-                    while(sketch_I[l] == redundant_sketch) {
-                        node_infl[node_I[l]] = node_infl[node_I[l]] - 1;
-                        sketch_I[l] = -1;
-                        node_I[l] = -1;
-                        l++;
-                    }
-                    if(j>0) // (j!=0) Boundary of the arrays sketch_I and node_I
-                    {
-                        int ll = j-1;
-                        while(sketch_I[ll] == redundant_sketch) {
-                            node_infl[node_I[ll]] = node_infl[node_I[ll]] - 1;
-                            sketch_I[ll] = -1;
-                            node_I[ll] = -1;
-                            ll--;
-                        }
-                    }
-                    sketch_I[j] = -1;
-                    node_I[j] = -1;
-                    sketch_left = sketch_left - 1;
-                }
+                I.get(u).removeAll(I.get(max_node));
             }
         }
-        node_infl[max_node] = 0;
+        I.get(max_node).clear();
 
-        get_seeds(sketch_I, node_I, node_infl, k-1, count_sketches, sketch_num, sketch_left);
+        get_seeds(I, k-1, sketch_num);
     }
 
 
@@ -255,10 +202,10 @@ public class InfluenceMax_flat {
         String basename  = args[0];
         double p = Double.valueOf(args[1]);
 
-        InfluenceMax_flat imfl = new InfluenceMax_flat(basename, p);
-        imfl.get_sketch();
+        InfluenceMax_list iml = new InfluenceMax_list(basename, p);
+        iml.get_sketch();
 
         estimatedTime = System.currentTimeMillis() - startTime;
-        System.out.println("Time elapsed = " + estimatedTime /(1000.0) + " sec");
+        System.out.println("Time elapsed = " + estimatedTime / 1000.0 + " sec");
     }
 }
